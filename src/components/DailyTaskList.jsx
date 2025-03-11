0e6c4206780c09777c1c3157fd2068564a26943f
Travis Oliver <olivertravis554@gmail.com>
2025-03-10 23:39:20 -0400

    mobile styling

--- src/components/DailyTaskList.jsx
+++ src/components/DailyTaskList.jsx
@@ -1,20 +1,17 @@
 function DailyTaskList() {
-	return (
-		<div className="task-list-container">
-			<div className='task-list'>
-			<h1>Todays Tasks</h1>
-			<ul>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-				<li>Clean trash</li>
-			</ul>
-		</div>
-		</div>
-		
-	);
+  return (
+    <div className='task-list-container'>
+      <div className='task-list'>
+        <h1>Todays Tasks</h1>
+        <ul>
+          <li>Clean trash</li>
+          <li>Replace ropes</li>
+          <li>Organize tool shed</li>
+          <li>Wipe down the coolers</li>
+        </ul>
+        <button className='task-list-submit'>Daily task list</button>
+      </div>
+    </div>
+  );
 }
 export default DailyTaskList;
--- src/pages/Harnesses.jsx
+++ src/pages/Harnesses.jsx
@@ -1,13 +1,24 @@
 function Harnesses() {
-	return (
-		<div className="harness-container">
-			<div className='harness'>
-			<h2>Monthly Inspection</h2>
-			<h2>Repairs</h2>
-			<h2>Clic-it Parts</h2>
-		</div>
-		</div>
-		
-	);
+  return (
+    <div className='harness-container'>
+      <div className='harness'>
+        <div className='monthly-inspections'>
+          <h2>Monthly Inspection</h2>
+          <input type='text' placeholder='Wire condition' />
+          <br />
+          <input type='text' placeholder='Bolt condition' />
+          <br />
+          <input type='text' placeholder='Ropes condition' />
+          <br />
+          <input type='text' placeholder='Tape condition' />
+          <br />
+          <input type='text-area' placeholder='Additional comments' />
+        </div>
+
+        {/* <h2>Repairs</h2>
+        <h2>Clic-it Parts</h2> */}
+      </div>
+    </div>
+  );
 }
 export default Harnesses;
