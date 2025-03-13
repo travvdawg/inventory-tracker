170e09bfe7f86dd2b73db2abfb17c05175062db6
Travis Oliver <olivertravis554@gmail.com>
2025-03-12 21:48:15 -0400

    new forum and hamburger closes when new link is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,48 +4,55 @@ import Hamburger from 'hamburger-react';
 import Ropes from '../pages/Ropes';
 
 function Navbar() {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<div>
-			<nav className='navbar'>
-				<div className='nav-left'>
-					<Link
-						to='/'
-						className='logo'>
-						<img
-							src='https://www.orlandotreetrek.com/wp-content/uploads/2019/11/orlando-tree-trek-logo.png'
-							alt='OTTAP logo'
-						/>
-					</Link>
-				</div>
-				<div className='nav-right'>
-					<div className='hamburger-menu'>
-						<Hamburger
-							toggled={isOpen}
-							toggle={setIsOpen}
-						/>
-					</div>
-					<ul className={`nav-items ${isOpen ? 'active' : ''}`}>
-						<li>
-							<Link to='/harnesses'>Harnesses</Link>
-						</li>
-						<li>
-							<Link to='/wood'>Wood</Link>
-						</li>
-						<li>
-							<Link to='/courses'>Courses</Link>
-						</li>
-						<li>
-							<Link to='/hardware'>Hardware</Link>
-						</li>
-						<li>
-							<Link to='/ropes'>Ropes</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
-	);
+  const handleLinkClick = () => setIsOpen(false);
+
+  return (
+    <div>
+      <nav className='navbar'>
+        <div className='nav-left'>
+          <Link to='/' className='logo'>
+            <img
+              src='https://www.orlandotreetrek.com/wp-content/uploads/2019/11/orlando-tree-trek-logo.png'
+              alt='OTTAP logo'
+            />
+          </Link>
+        </div>
+        <div className='nav-right'>
+          <div className='hamburger-menu'>
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
+          </div>
+          <ul className={`nav-items ${isOpen ? 'active' : ''}`}>
+            <li>
+              <Link to='/harnesses' onClick={handleLinkClick}>
+                Harnesses
+              </Link>
+            </li>
+            <li>
+              <Link to='/wood' onClick={handleLinkClick}>
+                Wood
+              </Link>
+            </li>
+            <li>
+              <Link to='/courses' onClick={handleLinkClick}>
+                Courses
+              </Link>
+            </li>
+            <li>
+              <Link to='/hardware' onClick={handleLinkClick}>
+                Hardware
+              </Link>
+            </li>
+            <li>
+              <Link to='/ropes' onClick={handleLinkClick}>
+                Ropes
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
 export default Navbar;
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,12 +1,23 @@
 function Courses() {
-	return (
-		<div className="courses-container">
-			<div className='courses'>
-			<h2>Inspection</h2>
-			<h2>Repairs</h2>
-		</div>
-		</div>
-		
-	);
+  return (
+    <div className='courses-container'>
+      <div className='courses'>
+        <div className='monthly-inspection'>
+          <h2>Inspection</h2>
+          <input type='text' placeholder='Wire condition' />
+          <br />
+          <input type='text' placeholder='Bolt condition' />
+          <br />
+          <input type='text' placeholder='Ropes condition' />
+          <br />
+          <input type='text' placeholder='Tape condition' />
+          <br />
+          <input type='text-area' placeholder='Additional comments' />
+        </div>
+        <button className='task-list-submit'>Submit</button>
+      </div>
+      <h2>Repairs</h2>
+    </div>
+  );
 }
 export default Courses;
