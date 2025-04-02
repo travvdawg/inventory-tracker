72ea47cf15801dc0aa15735d8814f32ed41487ff
Travis Oliver <olivertravis554@gmail.com>
2025-04-01 22:03:12 -0400

    toastify notifications added to some components

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  // server: {
-  //   historyApiFallback: true,
-  // },
-  // preview: {
-  //   historyApiFallback,
-  // },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback,
+  },
 });
